fcc205b3b41ed188359cb81c0cfe8d083611279e
Grant <grant.tepper@gmail.com>
2013-07-08 00:24:34 -0400

    modified web.js to read index.html;

--- web.js
+++ web.js
@@ -1,12 +1,21 @@
 var express = require('express');
 
+var fs = require('fs');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    var page = function () { 
+        var file = 'assets/index.html';
+		var buff = fs.readFileSync(file);
+		
+		return (buff.toString());  
+	};
+
+	response.send(page(), 200);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
